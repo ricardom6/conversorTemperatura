6746e06aad1467be5aab32275ad24947ec2e88d1
Ricardo Menezes <ricardosilvademenezes@gmail.com>
2019-05-11 13:14:02 -0300

    style label e text do textField

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-  home: Home(),
-));
-
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    ));
 
 class Home extends StatefulWidget {
   @override
@@ -14,6 +13,10 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    TextStyle styleDecoration =
+        TextStyle(color: Colors.black87, fontSize: 20);
+    TextStyle styleField = TextStyle(color: Colors.lightBlueAccent);
+
     AppBar appBar = AppBar(
       title: Text("Conversor de Temperatura"),
       centerTitle: true,
@@ -24,35 +27,45 @@ class _HomeState extends State<Home> {
           onPressed: () {},
         )
       ],
-
     );
-    Icon icon = Icon(Icons.wb_sunny, size: 120, color: Colors.yellow,);
+    Icon icon = Icon(
+      Icons.wb_sunny,
+      size: 120,
+      color: Colors.yellow,
+    );
     TextField tempCelsius = TextField(
       decoration: InputDecoration(
-          labelText: 'Temp Celsius'
+        labelText: 'Temp Celsius',
+        labelStyle: styleDecoration,
       ),
-      keyboardType: TextInputType.number,);
+      keyboardType: TextInputType.number,
+      textAlign: TextAlign.center,
+      style: styleField,
+    );
     TextField tempFahrenheit = TextField(
       decoration: InputDecoration(
-          labelText: 'Temp Fahrenheit'
+        labelText: 'Temp Fahrenheit',
+        labelStyle: styleDecoration,
       ),
-      keyboardType: TextInputType.number,);
+      textAlign: TextAlign.center,
+      keyboardType: TextInputType.number,
+      style: styleField,
+    );
+
     Column column = Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
-        icon, tempCelsius, tempFahrenheit,
-
+        icon,
+        tempCelsius,
+        tempFahrenheit,
       ],
     );
 
-
-      Scaffold scaffold = Scaffold(
-        appBar: appBar,
-       backgroundColor: Colors.white,
-
-
-        body: column,
-        );
-      return scaffold;
-    }
+    Scaffold scaffold = Scaffold(
+      appBar: appBar,
+      backgroundColor: Colors.white,
+      body: column,
+    );
+    return scaffold;
   }
+}
