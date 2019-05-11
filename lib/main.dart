53f0e19d989a2d36130fd44b290f56b881544542
Ricardo Menezes <ricardosilvademenezes@gmail.com>
2019-05-11 11:04:46 -0300

    AppBar adicionada

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,23 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    AppBar appBar = AppBar(
+      title: Text("Conversor de Temperatura"),
+      centerTitle: true,
+      backgroundColor: Colors.blue,
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.refresh),
+          onPressed: () {},
+        )
+      ],
+
+    );
+
+
+      Scaffold scaffold = Scaffold(
+        appBar: appBar,
+        );
+      return scaffold;
+    }
   }
-}
